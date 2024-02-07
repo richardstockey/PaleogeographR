94146cd9e088b22d1f436956f40583431cee14c4
richardstockey <50849895+richardstockey@users.noreply.github.com>
2024-02-07 19:39:40 +0000

    Update scotese.map.simple.R

--- R/scotese.map.simple.R
+++ R/scotese.map.simple.R
@@ -30,6 +30,7 @@ scotese.map.simple <- function(age # Age in millions of years ago
   # box()
   max <- max(df)
   min <- min(df)
+  steps <- (max-min)/16
   levels_vec <- seq(min, max, 500)
   filled.contour(x = -180:180, y = -90:90, z = df, color.palette = function(n) rev(paletteer::paletteer_c("grDevices::Earth", 16)), xlab = "Longitude (°)", ylab = "Latitude (°)", levels = levels_vec)
 
